e54d9c38c72c58cb4d93b12d19129eb1797a7afa
Sagar Patel <17.sagar.patel@gmail.com>
2011-09-30 20:23:49 -0400

    Re-enabled exiting game by pressing back.

--- E-Shooter/E-Shooter/E-Shooter/Game1.cs
+++ E-Shooter/E-Shooter/E-Shooter/Game1.cs
@@ -92,8 +92,8 @@ namespace E_Shooter
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-           // if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-             //   this.Exit();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
 
 
             //if (InputManager.sharedInputManager.getFlickDelta().Length() > 0)
